ddfa75acf7e0066ea4c55ff90b4a02b254106861
Vincent <vbalalian@gmail.com>
2025-10-27 21:41:19 -0700

    add slack resource

--- dagster-project/src/dagster_project/defs/resources/__init__.py
+++ dagster-project/src/dagster_project/defs/resources/__init__.py
@@ -1,9 +1,11 @@
 from dagster import Definitions
 from dagster_project.defs.resources.gcp_resources import bq_resource, gcs_resource
+from dagster_project.defs.resources.slack_resource import slack_resource
 
 defs = Definitions(
     resources={
         "bigquery": bq_resource,
-        "gcs": gcs_resource
+        "gcs": gcs_resource,
+        "slack": slack_resource
     }
 )
