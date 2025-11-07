acb45ea8e523bea8c5a65e9492a9cce913d6ba7b
Vincent <vbalalian@gmail.com>
2025-11-06 23:06:18 -0800

    update frequency scoring logic

--- dbt-project/models/marts/dim_user_rfm.sql
+++ dbt-project/models/marts/dim_user_rfm.sql
@@ -11,6 +11,18 @@ source_dim_users as (
 
 ),
 
+ranked_users as (
+
+    select 
+
+        *,
+        dense_rank() over (order by purchase_count) as freq_percentile
+
+    from source_dim_users
+    where purchase_count > 0
+
+),
+
 rfm_scores as (
     select
 
@@ -20,11 +32,18 @@ rfm_scores as (
         round(total_revenue, 2) as total_revenue,
 
         ntile(5) over (order by days_since_last_purchase desc) as recency_score,
-        ntile(5) over (order by purchase_count) as frequency_score,
+
+        case
+            when freq_percentile < 0.2 then 1
+            when freq_percentile < 0.4 then 2
+            when freq_percentile < 0.6 then 3
+            when freq_percentile < 0.8 then 4
+            else 5
+        end as frequency_score,
+
         ntile(5) over (order by total_revenue) as monetary_score
 
-    from source_dim_users
-    where purchase_count > 0
+    from ranked_users
 ),
 
 final_users_rfm as (
