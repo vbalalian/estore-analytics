3c194ab43d768d5f379d5f542f5edddf40d1228b
vbalalian <vbalalian@gmail.com>
2025-11-14 13:01:28 -0800

    initial commit

---
+++ dbt-project/models/marts/metrics_conversion_rates.sql
@@ -0,0 +1,78 @@
+{{ config(
+    materialized='table'
+) }}
+
+with source_sessions as (
+    select * from {{ ref('fct_sessions') }}
+),
+
+overall_metrics as (
+    select
+        'overall' as metric_level,
+        'all_time' as time_period,
+        
+        count(distinct session_id) as total_sessions,
+        sum(reached_view) as sessions_with_view,
+        sum(reached_cart) as sessions_with_cart,
+        sum(reached_purchase) as sessions_with_purchase,
+        
+        round(safe_divide(
+            sum(reached_cart), 
+            sum(reached_view)
+        ), 5) as view_to_cart_rate,
+        
+        round(safe_divide(
+            sum(reached_purchase), 
+            sum(reached_cart)
+        ), 5) as cart_to_purchase_rate,
+        
+        round(safe_divide(
+            sum(reached_purchase), 
+            sum(reached_view)
+        ), 5) as view_to_purchase_rate,
+        
+        round(sum(total_revenue), 2) as total_revenue,
+        round(avg(case when reached_purchase = 1 then total_revenue end), 2) as avg_order_value
+        
+    from source_sessions
+),
+
+daily_metrics as (
+    select
+        'daily' as metric_level,
+        cast(session_start_date as string) as time_period,
+        
+        count(distinct session_id) as total_sessions,
+        sum(reached_view) as sessions_with_view,
+        sum(reached_cart) as sessions_with_cart,
+        sum(reached_purchase) as sessions_with_purchase,
+        
+        round(safe_divide(
+            sum(reached_cart), 
+            sum(reached_view)
+        ), 5) as view_to_cart_rate,
+        
+        round(safe_divide(
+            sum(reached_purchase), 
+            sum(reached_cart)
+        ), 5) as cart_to_purchase_rate,
+        
+        round(safe_divide(
+            sum(reached_purchase), 
+            sum(reached_view)
+        ), 5) as view_to_purchase_rate,
+        
+        round(sum(total_revenue), 2) as total_revenue,
+        round(avg(case when reached_purchase = 1 then total_revenue end), 2) as avg_order_value
+        
+    from source_sessions
+    group by session_start_date
+),
+
+final as (
+    select * from overall_metrics
+    union all
+    select * from daily_metrics
+)
+
+select * from final
