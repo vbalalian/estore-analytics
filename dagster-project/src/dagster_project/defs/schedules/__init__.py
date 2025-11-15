5329f1887f7a25eb3b2dc664071dd8ec7987ff1c
Vincent <vbalalian@gmail.com>
2025-11-14 21:25:26 -0800

    daily dbt cron job

---
+++ dagster-project/src/dagster_project/defs/schedules/__init__.py
@@ -0,0 +1,9 @@
+from dagster import ScheduleDefinition, DefaultScheduleStatus
+from dagster_project.defs.jobs import dbt_job
+
+
+daily_pipeline_schedule = ScheduleDefinition(
+    job=dbt_job,
+    cron_schedule="0 0 * * *",  # every day, at midnight
+    default_status=DefaultScheduleStatus.RUNNING,
+)
