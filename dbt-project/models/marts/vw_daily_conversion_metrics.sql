c62ff8d0a94a30015c3b87295a21f1ae039e424e
vbalalian <vbalalian@gmail.com>
2025-11-21 17:37:25 -0800

    add view for daily metrics only

---
+++ dbt-project/models/marts/vw_daily_conversion_metrics.sql
@@ -0,0 +1,49 @@
+{{ config(materialized='view')}}
+
+with
+
+conversion_metrics as (
+
+    select * from {{ ref('metrics_conversion_rates') }}
+
+),
+
+daily_conversion_metrics as (
+
+    select
+
+        date(time_period) as metric_date,
+        total_sessions,
+        sessions_with_view,
+        sessions_with_cart,
+        sessions_with_purchase,
+        view_to_cart_rate,
+        cart_to_purchase_rate,
+        view_to_purchase_rate,
+        avg_order_value
+
+    from conversion_metrics
+
+    where metric_level = 'daily'
+    
+),
+
+final as (
+
+    select
+
+        metric_date,
+        total_sessions,
+        sessions_with_view,
+        sessions_with_cart,
+        sessions_with_purchase,
+        view_to_cart_rate,
+        cart_to_purchase_rate,
+        view_to_purchase_rate,
+        avg_order_value
+
+    from daily_conversion_metrics
+
+)
+
+select * from final
