469862981434645825fe1da3cd4ff69e43c07f74
Vincent <vbalalian@gmail.com>
2026-01-22 15:31:21 -0800

    Add terraform variables for dagster systemd setup
    Add deploy_user, repo_url, and slack_bot_token variables to support
    automated VM provisioning with dagster services. deploy_user defaults
    to 'dagster' (created by startup script), repo_url defaults to the
    public source repo.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,3 +19,21 @@ variable "gcs_bucket_name" {
   type        = string
   description = "Globally unique name for the GCS bucket used to store raw data."
 }
+
+variable "deploy_user" {
+  type        = string
+  description = "Unix user for deployment. Created by startup script if it doesn't exist."
+  default     = "dagster"
+}
+
+variable "repo_url" {
+  type        = string
+  description = "Git repository URL to clone for the estore-analytics project."
+  default     = "https://github.com/vbalalian/estore-analytics"
+}
+
+variable "slack_bot_token" {
+  type        = string
+  description = "Slack bot token for pipeline notifications."
+  sensitive   = true
+}
